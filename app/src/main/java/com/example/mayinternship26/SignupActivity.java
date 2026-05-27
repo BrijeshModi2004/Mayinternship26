81af9a5199ae21625f78694387b09e31c3118161
brijesh <modibrijesh601@gmail.com>
2026-05-27 16:42:51 +0530

    Signup Page Validation

--- app/src/main/java/com/example/mayinternship26/SignupActivity.java
+++ app/src/main/java/com/example/mayinternship26/SignupActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -17,6 +18,10 @@ public class SignupActivity extends AppCompatActivity {
 
     TextView already_account;
     Button button_sign_up;
+    EditText name, email, contact, password, cnf_password;
+    String EmailPattern = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +30,11 @@ public class SignupActivity extends AppCompatActivity {
 
         already_account = findViewById(R.id.already_account);
         button_sign_up = findViewById(R.id.button_sign_up);
+        name = findViewById(R.id.signup_name);
+        email = findViewById(R.id.signup_email);
+        contact = findViewById(R.id.signup_contact);
+        password = findViewById(R.id.signup_password);
+        cnf_password = findViewById(R.id.signup_confirm_password);
 
         already_account.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,7 +48,46 @@ public class SignupActivity extends AppCompatActivity {
         button_sign_up.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(SignupActivity.this, "User Registered Succesfully!!", Toast.LENGTH_SHORT).show();
+
+                 if (name.getText().toString().equals("")){
+                     name.setError("Name is Required");
+                 }
+                 
+                 else if(email.getText().toString().equals("")){
+                     email.setError("Email is Required");
+                 }
+                 
+                 else if (!email.getText().toString().matches(EmailPattern)) {
+                     email.setError("Email Not Valid");
+                 }
+                 
+                 else if (contact.getText().toString().equals("")) {
+                     contact.setError("Contact is Required");
+                 }
+
+                 else if (contact.getText().toString().length()<10) {
+                     contact.setError("Invalid Contact Number");
+                 }
+
+                 else if (password.getText().toString().equals("")){
+                     password.setError("Password is Required");
+                 }
+
+                 else if (password.getText().toString().length()<8) {
+                     password.setError("Minimum 8 Characters Required");
+                 }
+
+                 else if (cnf_password.getText().toString().equals("")){
+                     cnf_password.setError("Confirm Password is Required");
+                 }
+
+                 else if (!cnf_password.getText().toString().matches(password.getText().toString())) {
+                     cnf_password.setError("Password does not matches");
+                 }
+
+                 else {
+                     Toast.makeText(SignupActivity.this, "User Registered Succesfully!!", Toast.LENGTH_SHORT).show();
+                 }
             }
         });
     }
